c117fc6586f759f8d893aaf635bead630579e326
Naratip Udomsilp <naratip.ud@gmail.com>
2017-08-23 14:29:41 +0700

    :sparkles: Add logout feature

--- ng-auth/src/app/dashboard/dashboard.component.ts
+++ ng-auth/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from 'app/shared/services';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+  ) { }
 
   ngOnInit() {
+    this.userService.currentUser.subscribe((user) => this.currentUser = user);
+  }
+
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateByUrl('/');
   }
 
 }
--- ng-auth/src/app/shared/layout/header.component.ts
+++ ng-auth/src/app/shared/layout/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { UserService } from '../services';
 import { User } from '../models/user.model';
 
@@ -9,10 +11,18 @@ import { User } from '../models/user.model';
 export class HeaderComponent implements OnInit {
   currentUser: User;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
-    this.userService.currentUser.subscribe((userData) => this.currentUser = userData);
+    this.userService.currentUser.subscribe((user) => this.currentUser = user);
+  }
+
+  logout() {
+    this.userService.purgeAuth();
+    this.router.navigateByUrl('/');
   }
 
 }
